4956ff4027e1bb31d4d29c1803840c68b0fde241
LC Perri <prluisca@gmail.com>
2025-10-17 00:30:15 -0500

    mejoras a las cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const ProjectCard = ({ key, title, description, tech, gifUrl, projectUrl }) => {
   return (
-    <div className="flex flex-wrap lg:flex-nowrap w-[94%] md:w-140 lg:w-180 text-whit hover:scale-105 transition-transform
+    <div className="flex flex-wrap lg:flex-nowrap w-[94%] md:w-140 lg:w-180 hover:scale-105 transition-transform
     relative p-[3px] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-[rgb_3s_linear_infinite] shadow-[0_0_20px_rgba(236,72,153,0.6)]">
-        <div className="lg:flex gap-4 p-8 bg-black/90 backdrop-blur-md text-white">
-          <div index={key} className='flex flex-col gap-4 lg:w-[55%]' >
-            <h3 className="font-black text-xl mb-2">{title.toUpperCase()}</h3>
+        <div className="lg:flex gap-5 p-8 bg-black/90 backdrop-blur-md text-white ">
+          <div index={key} className='flex flex-col gap-4 lg:w-[55%] ' >
+            <h3 className="font-black text-[16px] md:text-xl mb-2">{title.toUpperCase()}</h3>
             <p className="">{description}</p>
             <div className="flex flex-wrap">
               {tech.map((t, index) => (
-                <span index={index} className="text-sm mt-2">
+                <span index={index} className="text-xs mt-2">
                   - {t}&nbsp;
                 </span>
               ))}
@@ -20,10 +20,10 @@ const ProjectCard = ({ key, title, description, tech, gifUrl, projectUrl }) => {
             <img 
               src={gifUrl}
               alt={title} 
-              className="rounded-2xl md:w-110 md:h-65 lg:w-100 lg:h-54"
+              className="rounded-2xl md:w-100 md:h-65 lg:w-95 lg:h-54 mt-8 mx-auto"
             />
-            <a href={projectUrl} target="_blank" rel="noopener noreferrer" className="w-40 md:bottom-10 md:p-3 lg:p-3 bg-red-400 md:bg-white border-2 
-            border-white px-4 py-2 text-black/80 rounded-md shadow-md hover:bg-red-400 transition text-center">
+            <a href={projectUrl} target="_blank" rel="noopener noreferrer" className="md:bottom-4 p-4 bg-red-400 md:bg-white border-2 
+            border-white text-black/80 rounded-md shadow-md hover:bg-red-400">
               Ver proyecto
             </a>
           </div>
