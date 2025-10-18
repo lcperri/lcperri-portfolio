27c63c797848db3bd978c104f6d71eee0e999090
LC Perri <prluisca@gmail.com>
2025-10-17 21:11:23 -0500

    fixing vite pages client side para vercel

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import { Route, Routes } from 'react-router-dom'
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/projects' element={<Projects />} />
     </Routes>
   )
 }
